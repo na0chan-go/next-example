6610897a8396c04cf6dd7ede3dc22c707c024754
na0chan-go <nn19960218@gmail.com>
2024-09-02 23:19:46 +0900

    feat: server componentとclient componentの違いについて確認

---
+++ src/app/sc/page.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const ServerComponent = () => {
+  console.log("Server Component");
+  //   useState();
+  return <div>ServerComponent</div>;
+};
+
+export default ServerComponent;
