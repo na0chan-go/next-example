7d280e3ee6d75cc02ccaf96d38b2a9d0c4de9ad3
na0chan-go <nn19960218@gmail.com>
2024-09-03 00:45:49 +0900

    feat: ルートハンドラーの挙動の確認

--- src/app/(main)/task/page.tsx
+++ src/app/(main)/task/page.tsx
@@ -1,5 +1,24 @@
-const TaskPage = () => {
-  return <div>TaskPage</div>;
+import { Task } from "@/app/api/tasks/route";
+
+const getTasks = async () => {
+  const response = await fetch("http://localhost:3000/api/tasks", {
+    method: "GET",
+    cache: "no-store",
+  });
+  return await response.json();
+};
+const TaskPage = async () => {
+  const tasks = (await getTasks()).tasks as Task[];
+  return (
+    <div>
+      <div>TaskPage</div>
+      <div>
+        {tasks.map((tasks) => (
+          <div key={tasks.id}>{tasks.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TaskPage;
